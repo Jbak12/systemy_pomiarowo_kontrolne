20b0d39c3d47125953651d9b86170220070807d2
Jakub Bakalarz <87420610+Jbak12@users.noreply.github.com>
2023-03-07 17:58:41 +0100

    zajecia_2 v2

---
+++ przerwania/przerwania.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+  TCCR1A = 0;
+  TCCRIB = 5;
+  TIMSK = (1<<TO1E1)
+  SREG = SREG|128
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
---
+++ sensor/sensor.ino
@@ -0,0 +1,30 @@
+#define trigPin 12
+#define echoPin 11
+ 
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT); //Pin, do którego podłączymy trig jako wyjście
+  pinMode(echoPin, INPUT); //a echo, jako wejście
+}
+ 
+void loop() {  
+  Serial.print(zmierzOdleglosc());
+  Serial.println(" cm");
+  
+  delay(10);
+} 
+ 
+int zmierzOdleglosc() {
+  long czas, dystans;
+ 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, MLOW);
+ 
+  czas = pulseIn(echoPin, HIGH);
+  dystans = czas / 58;
+ 
+  return dystans;
+}
