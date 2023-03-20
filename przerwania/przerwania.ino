cdb86ab33597cdd2340f86a6d68ad7969d1a5149
Jakub Bakalarz <87420610+Jbak12@users.noreply.github.com>
2023-03-20 11:36:13 +0100

    zajecia_3-przerwania i dalmierz

--- przerwania/przerwania.ino
+++ przerwania/przerwania.ino
@@ -1,14 +1,23 @@
+int n = 0;
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin (9600);
   TCCR1A = 0;
-  TCCRIB = 5;
-  TIMSK = (1<<TO1E1)
-  SREG = SREG|128
+  TCCR1B = 1;
+  // dla tccrib 2, n zwiekszane 15 razy na sek, dla 4- 1, dla 5- 0, dla 3- 2, dla 1- 122
+  TIMSK = (1<<TOIE1)|TIMSK;
+  SREG = SREG|128;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
+  Serial.println(n);
+  n = 0;
+  delay(1000);
   
 }
+
+ISR(TIMER1_OVF_vect) {
+  n++;
+}  
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,30 +1,67 @@
 #define trigPin 12
 #define echoPin 11
- 
+#define soundConstant 58
+
+byte tab[] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
+byte loop_values[] = {B11110111,B1111011,B11111101,B11111110};
+int digits[] = {0,0,0,0} ;
+int dist; 
 void setup() {
+  
+  //ustawianie setupu do przerwan
+  TCCR1A = 0;
+  TCCR1B = 1;
+  // dla tccrib 2, n zwiekszane 15 razy na sek, dla 4- 1, dla 5- 0, dla 3- 2, dla 1- 122
+  TIMSK = (1<<TOIE1)|TIMSK;
+  SREG = SREG|128;
+  
+  //ustawianie portow wyjscia i data direction
+  DDRB = 255;
+  DDRA = 255;
+  PORTB = ~63;
   Serial.begin (9600);
-  pinMode(trigPin, OUTPUT); //Pin, do którego podłączymy trig jako wyjście
-  pinMode(echoPin, INPUT); //a echo, jako wejście
+  pinMode(trigPin, OUTPUT); //Pin, do ktĂłrego podĹ‚Ä…czymy trig jako wyjĹ›cie
+  pinMode(echoPin, INPUT); //a echo, jako wejĹ›cie
 }
  
-void loop() {  
-  Serial.print(zmierzOdleglosc());
+void loop() {
+
+  dist = zmierzOdleglosc();
+  Serial.print(dist);
+  initialise_array(digits,4,dist);
   Serial.println(" cm");
   
+  for(int i = 0; i < 4; i ++) {
+    PORTA = loop_values[i];
+    PORTB = ~tab[digits[i]];
+    delay(2);
+     }
+    
   delay(10);
 } 
  
 int zmierzOdleglosc() {
-  long czas, dystans;
+  int czas, dystans;
  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, MLOW);
+  digitalWrite(trigPin, LOW);
  
   czas = pulseIn(echoPin, HIGH);
-  dystans = czas / 58;
+  dystans = czas / soundConstant;
  
   return dystans;
 }
+
+void initialise_array(int *tab, int size, int num) {
+    for (int i = 0; i < size; ++i, num /= 10)
+        tab[(size - 1) - i] = num % 10;
+}
+
+ISR(TIMER1_OVF_vect) {
+  
+  
+}
+
